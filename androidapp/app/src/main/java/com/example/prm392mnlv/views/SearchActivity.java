b619acfd13593a6e18c6a1c041119e8965067c7e
bacsiemon <johnsieunhan816@gmail.com>
2025-03-03 11:53:50 +0700

    empty views

--- androidapp/app/src/main/java/com/example/prm392mnlv/views/SearchActivity.java
+++ androidapp/app/src/main/java/com/example/prm392mnlv/views/SearchActivity.java
@@ -1,4 +1,4 @@
-package com.example.prm392mnlv;
+package com.example.prm392mnlv.views;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.prm392mnlv.R;
+
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_search);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
