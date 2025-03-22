78bf6e61563dae5429335b68ee9c16fa2e47f783
bacsiemon <johnsieunhan816@gmail.com>
2025-03-22 16:45:57 +0700

    feat : home screen navigation bar

--- androidapp/app/src/main/java/com/example/prm392mnlv/ui/activities/HomeActivity.java
+++ androidapp/app/src/main/java/com/example/prm392mnlv/ui/activities/HomeActivity.java
@@ -1,16 +1,23 @@
 package com.example.prm392mnlv.ui.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.prm392mnlv.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,29 @@ public class HomeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        configureNavBar();
+    }
+
+    private void configureNavBar(){
+        navigationView = findViewById(R.id.bottom_nav_menu);
+        navigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.item_home) {
+                Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
+            }
+
+            if (item.getItemId() == R.id.item_cart) {
+                Toast.makeText(HomeActivity.this, "Cart", Toast.LENGTH_SHORT).show();
+            }
+
+            if (item.getItemId() == R.id.item_notification) {
+                Toast.makeText(HomeActivity.this, "Notification", Toast.LENGTH_SHORT).show();
+            }
+
+            if (item.getItemId() == R.id.item_account) {
+                Toast.makeText(HomeActivity.this, "Account", Toast.LENGTH_SHORT).show();
+            }
+            return true;
         });
     }
 }
