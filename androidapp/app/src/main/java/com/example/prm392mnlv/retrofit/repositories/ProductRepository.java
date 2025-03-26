78cd44b020e728dd1bbfa854da1555c283989901
0767534208 <quanghuy010503@gmail.com>
2025-03-26 13:33:01 +0700

    Filter Product

--- androidapp/app/src/main/java/com/example/prm392mnlv/retrofit/repositories/ProductRepository.java
+++ androidapp/app/src/main/java/com/example/prm392mnlv/retrofit/repositories/ProductRepository.java
@@ -3,6 +3,7 @@ package com.example.prm392mnlv.retrofit.repositories;
 import androidx.annotation.Nullable;
 
 import com.example.prm392mnlv.data.dto.response.ProductResponse;
+import com.example.prm392mnlv.data.models.Category;
 import com.example.prm392mnlv.retrofit.client.ApiClient;
 import com.example.prm392mnlv.retrofit.services.ProductService;
 
@@ -19,6 +20,12 @@ public class ProductRepository {
 
     public void getProducts(@Nullable String id, @Nullable String productName, @Nullable String categoryName, Callback<List<ProductResponse>> callback) {
         mProductService.getProducts(id, productName, categoryName, 1, 999)
+                .enqueue(callback);
+    }
+
+    // Hàm mới: Lấy danh sách category
+    public void getCategories(Callback<List<Category>> callback) {
+        mProductService.getCategories()
                 .enqueue(callback);
     }
 }
